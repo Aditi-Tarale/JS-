77a7eeb2933f2259f99e21bf7b3257dbe9456040
Aditi-Tarale <adititarale20@gmail.com>
2025-04-10 07:19:16 +0000

    add

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,13 @@
+const accountId =14215
+let accountEmail="aditi@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+//accountId = 2 //It is does not change
+accountEmail = "ad@ad.com"
+accountPassword="2019"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
