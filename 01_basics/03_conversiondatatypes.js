5d5f72fb9c4db6d0e02f4fda72f9deab464409db
Aditi-Tarale <adititarale20@gmail.com>
2025-04-11 07:35:26 +0000

    add

--- 01_basics/03_conversiondatatypes.js
+++ 01_basics/03_conversiondatatypes.js
@@ -22,4 +22,48 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNum = 33
 let StringNum = String(someNum)
-console.log(typeof StringNum);
+// console.log(typeof StringNum);
+
+
+// ************************Operation************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 ="hello";
+let str2 = " Aditi";
+
+let str3 = str1 + str2;
+// console.log(str3);
+ 
+// console.log("1" + 2);
+// console.log(1+"2");
+// console.log("1"+2+2)
+// console.log(1+2+"2");
+
+// console.log((3 + 4 ) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
---
+++ 01_basics/04_datatypes-summary.js
@@ -0,0 +1,43 @@
+// Primitive
+
+// 7 types : String, Number, Null, undefined,Boolean,Symbol,BigInt
+
+// Dynamic sorce
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id =Symbol('123')
+const anotherId =Symbol('123')
+
+// console.log(id == anotherId);
+
+const bigNum = 345759370957359720379520n
+
+
+ 
+
+//Reference(Non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj","doga"]
+
+let myObj =
+{
+    name:"Aditi",
+    age:20
+}
+
+const myFunct = function()
+{
+    console.log("Hello world");
+    
+}
+
+
+console.log(typeof myFunct);
