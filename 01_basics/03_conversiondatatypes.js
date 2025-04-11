c3bd1b8bcd288e695cca98cdc89743f7a93fc16a
Aditi-Tarale <adititarale20@gmail.com>
2025-04-11 07:12:41 +0000

    Add

---
+++ 01_basics/03_conversiondatatypes.js
@@ -0,0 +1,25 @@
+// let score="Aditi"
+
+// // console.log(typeof score)
+
+// let valueInnumber = Number (score)
+// // console.log(typeof valueInnumber);
+// // console.log(valueInnumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 
+
+let isLoggedIn = "Aditi"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Aditi" => true
+
+
+let someNum = 33
+let StringNum = String(someNum)
+console.log(typeof StringNum);
