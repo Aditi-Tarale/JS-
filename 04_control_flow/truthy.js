6f15dd1f79509b1a6fb28c7b8e4beb02c2d68c5d
Aditi-Tarale <adititarale20@gmail.com>
2025-04-22 06:21:41 +0000

    control flow

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,47 @@
+const userEmail = []
+if(userEmail)
+{
+    console.log("Got user email");
+}
+else
+{
+    console.log("Don't have user email");
+    
+}
+
+
+// if(userEmail.length === 0)
+// {
+//     console.log("Array is empty");
+    
+// }
+
+const empObj = {}
+
+if(Object.keys(empObj).length === 0)
+{
+    console.log("Object is empty");
+    
+}
+
+
+//Nullish Coalescing Operator (??) : null undefined
+
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+
+console.log(val1);
+
+
+//Terniary Operator
+//condition ? true : false
+
+const iceteaPrice = 100
+iceteaPrice <= 80 ?console.log("less than 80"):console.log("more than 80");
+
+
